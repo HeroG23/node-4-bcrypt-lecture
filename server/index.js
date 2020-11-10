73a77a673d450dbb465fde4ac88774168e2861cd
Brian Demro <demrobrian@hotmail.com>
2020-11-10 12:51:19 -0600

    stuff

--- server/index.js
+++ server/index.js
@@ -2,20 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
 const session = require('express-session');
+// require session
 const auth = require('./authController');
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const app = express();
 // Top level middleware
 app.use(express.json());
+
 app.use(session({
-    resave:false,
+    resave: false,
     saveUninitialized: true,
     secret: SESSION_SECRET,
-    cookie: {maxAge: 
-        604800000
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }))
+
+app.use((req, res, next) => {
+    console.log('Custom top level hit!')
+    next();
+})
 
 massive({
     connectionString: CONNECTION_STRING,
@@ -28,5 +35,9 @@ massive({
 }).catch( err => console.log(err));
 
 // Enpoints
+app.post('/auth/register', auth.register);
+app.post('/auth/login', auth.login);
+app.post('/auth/logout', auth.logout);
+app.get('/auth/get_user', auth.getUser);
 
 app.listen(SERVER_PORT, () => console.log(`Connected to port ${SERVER_PORT}⛵⚓`))
